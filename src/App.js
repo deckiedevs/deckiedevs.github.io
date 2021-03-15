c308cf60e10d2e6e981968fa9f399bc519e10687
Mila Decker <milacdecker@gmail.com>
2021-03-14 21:25:51 -0400

    initialized React

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import About from './components/About';
+import Nav from './components/Nav';
+
+function App() {
+    return (
+        <div>
+            <Nav />
+            <main>
+                <About />
+            </main>
+        </div>
+    );
+}
+
+export default App;
