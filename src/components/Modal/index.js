0738a502949b8399afab470a7b95bb9b9ee0f8d4
Mila Decker <milacdecker@gmail.com>
2021-03-18 18:11:59 -0400

    formatted modal display

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -5,10 +5,12 @@ import '../../assets/css/modal.css';
 
 function Modal({ onClose, displayedProject }) {
     const {
-        link,
-        text,
         name,
+        text,
         technologies,
+        deployed,
+        link,
+        github,
         description
     } = displayedProject;
 
@@ -16,11 +18,28 @@ function Modal({ onClose, displayedProject }) {
         <div>
             <div className="modal-bg" onClick={onClose}></div>
             <div className="modal-card w100 px-5 py-5">
-                <h3>{name}</h3>
-                <p>
-                    {description}
-                </p>
                 <FontAwesomeIcon icon={faTimes} size="lg" onClick={onClose} className="modal-btn" />
+                <div className="modal-title">
+                    <h3>{name}</h3>
+                </div>
+                <div className="modal-body">
+                    <p>
+                        <span className="bold">Built With: </span> {technologies}
+                    </p>
+                    { deployed && 
+                        <p>
+                            <span className="bold">Deployed Site: </span> 
+                            <a href={link} target="_blank">{name}</a>
+                        </p>
+                    }
+                    <p>
+                        <span className="bold">GitHub: </span> 
+                        <a href={github} target="_blank">{name} Repository</a>
+                    </p>
+                    <p>
+                        <span className="bold">Description: </span> {description}
+                    </p>
+                </div>
             </div>
         </div>
     );
